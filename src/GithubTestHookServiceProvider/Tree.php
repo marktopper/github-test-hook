b3bdfb0e633252822a21637df38f09c50b511583
Mark Topper <mark@ulties.com>
2016-12-20 08:57:05 +0100

    Boot instead of register

--- src/GithubTestHookServiceProvider/Tree.php
+++ src/GithubTestHookServiceProvider/Tree.php
@@ -4,7 +4,7 @@ namespace GithubTestHookServiceProvider;
 
 class Tree extends \Illuminate\Support\ServiceProvider
 {
-  public function register(\Illuminate\Routing\Router $router)
+  public function boot(\Illuminate\Routing\Router $router)
   {
     $router->get('test-3', function () {
       return 'GithubTestHookServiceProvider\Tree';
