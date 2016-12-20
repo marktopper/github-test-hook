0baed3b3b2c890334fb771a3b5420c3f55e36a66
Mark Topper <mark@ulties.com>
2016-12-20 08:56:34 +0100

    Boot instead of register

--- src/GithubTestHookServiceProviderTwo.php
+++ src/GithubTestHookServiceProviderTwo.php
@@ -2,7 +2,7 @@
 
 class GithubTestHookServiceProviderTwo extends \Illuminate\Support\ServiceProvider
 {
-  public function register(\Illuminate\Routing\Router $router)
+  public function boot(\Illuminate\Routing\Router $router)
   {
     $router->get('test-2', function () {
       return 'GithubTestHookServiceProviderTwo';
